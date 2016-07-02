3bd967224e8e4c8ec3088d9296aa50843036124f
wanghb <whb_196@163.com>
2016-07-02 19:55:41 +0800

    filter files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\JetBrains WebStorm\zjapp\out-android\zjwebapp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-E:\JetBrains WebStorm\zjapp\out-android\zjwebapp\platforms\android\CordovaLib\AndroidManifest.xml \
